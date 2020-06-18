777fa8e34df9f55bb9e8e9413e8035518bab928e
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2020-06-18 16:41:51 +0530

    :sparkles: feat: comfigure app with bootstrap, and add content

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -3,10 +3,29 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Rishav Pandey',
+        'title': 'Blog Post',
+        'content': 'First post content',
+        'date_posted': 'June 18, 2020'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post',
+        'content': 'Second post content',
+        'date_posted': 'June 19, 2020'
+    }
+]
+
 
 def home(request):
-    return render(request, "blog/home.html")
+    context = {
+        'posts': posts,
+        'title': "Home"
+    }
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return render(request, "blog/about.html")
+    return render(request, "blog/about.html", {'title': "About"})
